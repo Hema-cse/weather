e1a1ca388b7de36e42b475ff02921511f4438f42
Hema Dharmala <68768160+Hema-cse@users.noreply.github.com>
2021-11-22 16:56:51 +0530

    added the drop downs  to retreive the cities and calculated the temperature

--- Script1.js
+++ Script1.js
@@ -1,44 +1,112 @@
+var countries = [];
+getCitiesData();
+// retrieving the countries json file
+async function fetch_countriesData()
+{
+    const countries_res = await fetch("https://raw.githubusercontent.com/dr5hn/countries-states-cities-database/master/countries%2Bstates%2Bcities.json");
+    const countries_data = await countries_res.json();
+    countries = countries_data;
+    console.log(countries_data);
 
+}
 
+// retreiving few cities in each drop down
+async function getCitiesData()
+{
+    //calling the countries json file
+    await fetch_countriesData();
+    var cities1 = document.getElementById("cities1");
+    var cities2 = document.getElementById("cities2");
 
+    var citiCount = 0;
+    var c = 0;
+    for (var i = 0; i < countries.length; i++)
+    {
+        if (citiCount > 10)
+        {
+            break;
+        }
+        c=0;
+        var states = countries[i]['states'];
+        for (var j = 0; j < states.length; j++)
+        {
+           
+            if (citiCount > 10) {
+                break;
+            }
+            var cities = states[j]['cities'];
+            for (var k = 0; k < cities.length; k++)
+            {
+                if (++c>1)
+                {
+                    break;
+                }
+                if (++citiCount > 10)
+                {
+                    break;
+                }
+                // adding the options to 1 drop down
+                var option1 = document.createElement("OPTION");
+                option1.value = cities[k].latitude + "," + cities[k].longitude + "," + cities[k].name;
+                option1.innerHTML = cities[k].name;
+                cities1.options.add(option1);
 
-
-getData()
-    .catch(error => {
-        console.log('error occured while loading the data');
-        console.error(error);
-    });
-
-let states = [];
-let opt = [];
-let w=[]
-async function getData() {
-
-    
-    const countries = await fetch("https://raw.githubusercontent.com/dr5hn/countries-states-cities-database/master/countries%2Bstates%2Bcities.json");
-    const countries_data = await countries.json();
-   
-   console.log(countries_data);
-
-    
-    for (let i = 0; i < countries_data.length; i++) {
-        for (let j = 0; j < countries_data[i].states.length; j++) {
-             for (let k = 0; k < countries_data[i].states[j].cities.length; k++) {
-            //sel = document.createElement("select");
-            //opt = document.createElement("option");
-            //sel.options.add(opt);
-                 console.log(countries_data[i].states[j].cities[k]);
+                // adding the options to 2 drop down
+                var option2 = document.createElement("OPTION");
+                option2.value = cities[k].latitude + "," + cities[k].longitude + "," + cities[k].name;
+                option2.innerHTML = cities[k].name;
+                cities2.options.add(option2);
             }
         }
-        }
-         //document.CreateElement("option");
-
     }
 
-getData();
+}
+// loading the product astro json file
+async function fetch_time()
+{
+    const response = await fetch("https://www.7timer.info/bin/astro.php?lon=" + citi1value[1] + "&lat=" + citi1value[0] + "&ac=0&unit=metric&output=json&tzshift=0");
+    const latlong_data = await response.json();
+}
 
-/*let dropdown = document.getElementById('dropdown');
+/* returns the temperature of 2 cities
+ */
+async function compareTemp()
+{
+    var citi1value = document.getElementById("cities1");
+    citi1value = citi1value.options[citi1value.selectedIndex].value;
+    citi1value = citi1value.split(",");
+    var citi2value = document.getElementById("cities2");
+    citi2value = citi2value.options[citi2value.selectedIndex].value;
+    citi2value = citi2value.split(",");
+
+     // retreiving the timezones of 2 cities
+
+   
+    /* (t1 < t2) {
+        document.getElementsByClassName("temp_values1").innerHTML = "<span style='background-color: red;border-radius:50px;height:200px;width:200px'>**Message</span>";
+    }*/
+
+    // getting the temperature of city 1
+    const response = await fetch("https://www.7timer.info/bin/astro.php?lon=" + citi1value[1] + "&lat=" + citi1value[0] + "&ac=0&unit=metric&output=json&tzshift=0");
+    const latlong_data = await response.json();
+
+    // getting the current date
+    const d = new Date();
+    let hour = d.getHours();
+    // retrieving the current temperature from the json file from the array of temperatures
+    let x = Math.floor(hour / 3);
+    city1Temp = latlong_data.dataseries[x].temp2m;
+    alert(city1Temp);
+    document.getElementById("city1Temp").innerHTML = city1Temp;
+    
+    // getting the temperature of city 2
+    const response1 = await fetch("https://www.7timer.info/bin/astro.php?lon=" + citi2value[1] + "&lat=" + citi2value[0] + "&ac=0&unit=metric&output=json&tzshift=0");
+    const latlong_data1 = await response1.json();;
+    city2Temp = latlong_data1.dataseries[x].temp2m;
+    alert(city2Temp);
+    document.getElementById("city2Temp").innerHTML = city2Temp; 
+   
+}
 
 
-let defaultOption = document.createElement('option');
-defaultOption.text = 'Choose a city';*/
+  
